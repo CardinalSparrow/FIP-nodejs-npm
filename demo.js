ba9e6e02dd3c9a0de7ca8b572fca6b09110386e5
John Offia-chukwu <132206047+CardinalSparrow@users.noreply.github.com>
2024-10-20 00:41:22 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,24 @@
 
+// Demo 1: Hello World in Node.js
+console.log("Hello, Node.js!");
+
+// Demo 2: Basic Web Server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
+// Demo 3: Using npm package (lodash)
+const _ = require('lodash');
+
+const numbers = [1, 2, 3, 4, 5];
+console.log("Original array:", numbers);
+console.log("Reversed array:", _.reverse(numbers));
+
